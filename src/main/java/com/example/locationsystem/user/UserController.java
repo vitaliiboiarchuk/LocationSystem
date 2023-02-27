1cbea3bc5f0dc0481ffafa44e702dbdacd2d05d7
vitaliiboiarchuk <boyarchuk.vitaliy@gmail.com>
2023-02-27 23:30:02 +0100

    added create user controller

---
+++ src/main/java/com/example/locationsystem/user/UserController.java
@@ -0,0 +1,25 @@
+package com.example.locationsystem.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/create-user")
+    @ResponseBody
+    public String createUser() {
+        User user = new User();
+        user.setName("user");
+        user.setPassword("user");
+        userService.saveUser(user);
+        return "user created";
+    }
+}
