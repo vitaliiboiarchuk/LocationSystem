6d29f06d33a5faa50283aa9701f311e797ac5dad
vitaliiboiarchuk <boyarchuk.vitaliy@gmail.com>
2023-02-27 22:54:06 +0100

    Added configure method

---
+++ src/main/java/com/example/locationsystem/admin/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.locationsystem.admin;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers("/**").authenticated()
+                .and().formLogin();
+        return http.build();
+    }
+}
