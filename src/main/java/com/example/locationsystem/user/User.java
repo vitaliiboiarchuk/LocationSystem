511b05ac0b35725bd59843b25d8156a14799d593
vitaliiboiarchuk <boyarchuk.vitaliy@gmail.com>
2023-02-27 23:24:21 +0100

    user service added

--- src/main/java/com/example/locationsystem/user/User.java
+++ src/main/java/com/example/locationsystem/user/User.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Set;
+
 
 @Entity
 @Getter
@@ -18,6 +20,13 @@ public class User {
     private String name;
 
     private String email;
+
+    private String password;
+
+    private int enabled;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles;
 
 
 }
