0ca26c48a7b1ab0f69917c3be62b1688b6405bba
vitaliiboiarchuk <boyarchuk.vitaliy@gmail.com>
2023-03-15 17:01:53 +0100

    first commit

---
+++ src/main/java/com/example/locationsystem/LocationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.locationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/locationsystem/LocationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.locationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
