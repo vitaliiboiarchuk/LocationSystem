8157a3909722d83cf596fe74eca8002ea49c5130
vitaliiboiarchuk <boyarchuk.vitaliy@gmail.com>
2023-07-15 11:37:10 +0200

    fix

--- src/main/java/com/example/locationsystem/user/UserController.java
+++ src/main/java/com/example/locationsystem/user/UserController.java
@@ -1,26 +1,21 @@
 package com.example.locationsystem.user;
 
-import com.example.locationsystem.annotation.GetAndValidUserId;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import java.util.concurrent.CompletableFuture;
 
 import com.example.locationsystem.exception.ControllerExceptions.*;
-import org.springframework.web.util.WebUtils;
 
 @RestController
 @Log4j2
